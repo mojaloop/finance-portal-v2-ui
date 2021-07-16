2769f49cb40e5a58e1e57a547bda37a7c6ab00e3
msk- <mattkingston@gmail.com>
2021-07-16 15:12:59 +0100

    Bump Skaffold

--- integration_test/default.nix
+++ integration_test/default.nix
@@ -21,6 +21,38 @@ let
     dontFixup = true;
   };
 
+  skaffold = nixpkgs.buildGoModule rec {
+    pname = "skaffold";
+    version = "1.28.0";
+
+    src = nixpkgs.fetchFromGitHub {
+      owner = "GoogleContainerTools";
+      repo = "skaffold";
+      rev = "v${version}";
+      sha256 = "007jq7c160m1vggib6sq1fr5m77bv9gccq8cfn8rh6sj7h57rpf4";
+    };
+
+    vendorSha256 = "0ggq6vz8na7sm37fb3xdgq161p82sfa13iz088y7whl1ip1n1wa8";
+
+    subPackages = ["cmd/skaffold"];
+
+    buildFlagsArray = let t = "github.com/GoogleContainerTools/skaffold/pkg/skaffold"; in  ''
+      -ldflags=
+        -s -w
+        -X ${t}/version.version=v${version}
+        -X ${t}/version.gitCommit=${src.rev}
+        -X ${t}/version.buildDate=unknown
+    '';
+
+    nativeBuildInputs = [ nixpkgs.installShellFiles ];
+
+    postInstall = ''
+      installShellCompletion --cmd skaffold \
+        --bash <($out/bin/skaffold completion bash) \
+        --zsh <($out/bin/skaffold completion zsh)
+    '';
+  };
+
 in
 
 
@@ -28,5 +60,5 @@ in
   nixpkgs.kubeconform
   nixpkgs.kubernetes-helm
   k3d
-  nixpkgs.skaffold
+  skaffold
 ]
