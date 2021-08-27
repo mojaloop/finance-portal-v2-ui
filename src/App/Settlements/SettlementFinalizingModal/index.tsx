061689ad81893db2e2a428b4d5340b089faf27a4
msk- <mattkingston@gmail.com>
2021-08-27 01:43:39 +0100

    Add modal

---
+++ src/App/Settlements/SettlementFinalizingModal/index.tsx
@@ -0,0 +1,64 @@
+import React, { FC } from 'react';
+import { ErrorBox, Modal, Spinner } from 'components';
+import connector, { ConnectorProps } from './connectors';
+import { SettlementStatus, FinalizeSettlementError, FinalizeSettlementErrorKind } from '../types';
+
+import './SettlementFinalizingModal.css';
+
+const SettlementFinalizingModal: FC<ConnectorProps> = ({
+  finalizingSettlement,
+  finalizingSettlementError,
+  onModalCloseClick,
+}) => {
+  function computeErrorMessage(err: FinalizeSettlementError) {
+    switch (err.type) {
+      case FinalizeSettlementErrorKind.SET_SETTLEMENT_PS_TRANSFERS_COMMITTED:
+      case FinalizeSettlementErrorKind.SET_SETTLEMENT_PS_TRANSFERS_RECORDED:
+      case FinalizeSettlementErrorKind.SET_SETTLEMENT_SETTLED:
+      case FinalizeSettlementErrorKind.SET_SETTLEMENT_PS_TRANSFERS_RESERVED: {
+        return `${err.type}: ${err.value.errorDescription} [CODE: ${err.value.errorCode}]`;
+      }
+      case FinalizeSettlementErrorKind.PROCESS_PAYEE_FUNDS_IN:
+      case FinalizeSettlementErrorKind.RESERVE_PAYER_FUNDS_OUT:
+      case FinalizeSettlementErrorKind.COMMIT_PAYER_FUNDS_OUT: {
+        // TODO: table
+        const details = err.value
+          .map((v) => `For participant: ${v.participant.name}. Error: ${v.apiResponse.errorDescription}.`)
+          .join('\n');
+        return `${err.type}:\n${details}`;
+      }
+      default: {
+        // Did you get a compile error here? This code is written such that if every
+        // case in the above switch state is not handled, compilation will fail.
+        // TODO: why does this have an error when every case is indeed handled?
+        // const exhaustiveCheck: never = err.type;
+        throw new Error('Unhandled error state');
+      }
+    }
+  }
+  const content = finalizingSettlementError ? (
+    <ErrorBox>{`Error finalizing settlement: ${computeErrorMessage(finalizingSettlementError)}`}</ErrorBox>
+  ) : (
+    <div className="finalizing-settlement">
+      <div>{`Finalizing settlement: ${finalizingSettlement?.id}.`}</div>
+      <br />
+      <div>{`State: ${finalizingSettlement?.state}.`}</div>
+      <br />
+      <Spinner size={20} />
+    </div>
+  );
+
+  return (
+    <Modal
+      title={`Finalizing settlement ${finalizingSettlement?.id}`}
+      width="1200px"
+      onClose={onModalCloseClick}
+      isCloseEnabled={finalizingSettlement?.state === SettlementStatus.Settled || finalizingSettlementError}
+      flex
+    >
+      {content}
+    </Modal>
+  );
+};
+
+export default connector(SettlementFinalizingModal);
