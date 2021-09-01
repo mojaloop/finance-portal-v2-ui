48ef896e8427c5d5b9a598fa3267e18dd9bdcca8
Matt Kingston <mattkingston@gmail.com>
2021-09-01 13:30:37 +0100

    Tidy and document integration tests (#41)
    * Tidy and document integration tests
    
    * Fix user config
    
    * README improvements
    
    * Only mount single file into wso2is-populate container
    
    * Fix integration test workflow
    
    * Minor docs fixes
    
    * Remove dotenv
    
    * Revert "Remove dotenv"
    
    This reverts commit 6cc3fa4c2cf4665e9431cf8d93108abaf7cb30fa.
    
    * Reinstate dotenv
    
    * Some reorganising to make local development a little friendlier
    
    * Add missing from previous commit
    
    * Move ingress to 'backend' manifests
    
    * Update README

--- webpack.config.js
+++ webpack.config.js
@@ -44,26 +44,9 @@ module.exports = {
     port: 3000,
     publicPath: '/',
     proxy: {
-      '/api/login': {
-        target: 'http://localhost:3002',
+      '/api': {
+        target: 'http://localhost:8000',
         secure: false,
-        pathRewrite: {
-          '^/api/login': '',
-        },
-      },
-      '/api/settlement': {
-        target: 'http://localhost:3007',
-        secure: false,
-        pathRewrite: {
-          '^/api/settlement': '',
-        },
-      },
-      '/api/portal-backend': {
-        target: 'http://localhost:3002',
-        secure: false,
-        pathRewrite: {
-          '^/api/portal-backend': '',
-        },
       },
     },
   },
