c304ab8fb9ad42f0a8ada3347acb2a6cf671f249
msk- <mattkingston@gmail.com>
2021-11-25 12:11:50 +0000

    Removed now-redundant tests. Updated test suite.

--- tests/unit/reportFinalization/settlementReportDeserialization.test.ts
+++ tests/unit/reportFinalization/settlementReportDeserialization.test.ts
@@ -17,6 +17,12 @@ test('simple_positive.xlsx', async () => {
         },
         "positionAccountId": 21,
         "balance": 1501000,
+        "row": {
+          "balance": 1501000,
+          "rowNumber": 7,
+          "switchIdentifiers": "11 21 mmdokdollar",
+          "transferAmount": -1500,
+        },
         "transferAmount": -1500
       },
       {
@@ -26,12 +32,24 @@ test('simple_positive.xlsx', async () => {
         },
         "positionAccountId": 19,
         "balance": 2200,
+        "row": {
+          "balance": 2200,
+          "rowNumber": 8,
+          "switchIdentifiers": "1 19 visionfund",
+          "transferAmount": 1000,
+        },
         "transferAmount": 1000
       },
       {
         "participant": {
           "id": 3,
           "name": "hana"
+        },
+        "row": {
+          "balance": 2200,
+          "rowNumber": 9,
+          "switchIdentifiers": "3 25 hana",
+          "transferAmount": 500,
         },
         "positionAccountId": 25,
         "balance": 2200,
@@ -47,8 +65,8 @@ test('simple_positive_2.xlsx', async () => {
   const report = await deserializeReport(f);
 
   expect(report).toEqual({
-    settlementId: 22,
-    entries: [
+    "settlementId": 22,
+    "entries": [
       {
         "participant": {
           "id": 1417,
@@ -56,7 +74,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 521,
         "balance": 8398905,
-        "transferAmount": -136411
+        "transferAmount": -136411,
+        "row": {
+          "rowNumber": 7,
+          "switchIdentifiers": "1417 521 fsp3748",
+          "balance": 8398905,
+          "transferAmount": -136411
+        },
       },
       {
         "participant": {
@@ -65,7 +89,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 1463,
         "balance": 6981333,
-        "transferAmount": -92910
+        "transferAmount": -92910,
+        "row": {
+          "rowNumber": 8,
+          "switchIdentifiers": "182 1463 fsp4833",
+          "balance": 6981333,
+          "transferAmount": -92910
+        },
       },
       {
         "participant": {
@@ -74,7 +104,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 3016,
         "balance": 2498583,
-        "transferAmount": 461888
+        "transferAmount": 461888,
+        "row": {
+          "rowNumber": 9,
+          "switchIdentifiers": "3052 3016 fsp4866",
+          "balance": 2498583,
+          "transferAmount": 461888
+        },
       },
       {
         "participant": {
@@ -83,7 +119,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 631,
         "balance": 3415533,
-        "transferAmount": -322740
+        "transferAmount": -322740,
+        "row": {
+          "rowNumber": 10,
+          "switchIdentifiers": "2655 631 fsp3950",
+          "balance": 3415533,
+          "transferAmount": -322740
+        },
       },
       {
         "participant": {
@@ -92,7 +134,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 4063,
         "balance": 3517620,
-        "transferAmount": 205150
+        "transferAmount": 205150,
+        "row": {
+          "rowNumber": 11,
+          "switchIdentifiers": "4750 4063 fsp188",
+          "balance": 3517620,
+          "transferAmount": 205150
+        },
       },
       {
         "participant": {
@@ -101,7 +149,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 3646,
         "balance": 4906501,
-        "transferAmount": 263231
+        "transferAmount": 263231,
+        "row": {
+          "rowNumber": 12,
+          "switchIdentifiers": "1271 3646 fsp1413",
+          "balance": 4906501,
+          "transferAmount": 263231
+        },
       },
       {
         "participant": {
@@ -110,7 +164,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 660,
         "balance": 1075738,
-        "transferAmount": -237125
+        "transferAmount": -237125,
+        "row": {
+          "rowNumber": 13,
+          "switchIdentifiers": "3767 660 fsp451",
+          "balance": 1075738,
+          "transferAmount": -237125
+        },
       },
       {
         "participant": {
@@ -119,7 +179,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 308,
         "balance": 6006609,
-        "transferAmount": -216809
+        "transferAmount": -216809,
+        "row": {
+          "rowNumber": 14,
+          "switchIdentifiers": "2392 308 fsp1432",
+          "balance": 6006609,
+          "transferAmount": -216809
+        },
       },
       {
         "participant": {
@@ -128,7 +194,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 1157,
         "balance": 5365762,
-        "transferAmount": -40419
+        "transferAmount": -40419,
+        "row": {
+          "rowNumber": 15,
+          "switchIdentifiers": "611 1157 fsp4867",
+          "balance": 5365762,
+          "transferAmount": -40419
+        },
       },
       {
         "participant": {
@@ -137,7 +209,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 4320,
         "balance": 7152006,
-        "transferAmount": 377483
+        "transferAmount": 377483,
+        "row": {
+          "rowNumber": 16,
+          "switchIdentifiers": "2669 4320 fsp3915",
+          "balance": 7152006,
+          "transferAmount": 377483
+        },
       },
       {
         "participant": {
@@ -146,7 +224,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 4213,
         "balance": 5469188,
-        "transferAmount": -392160
+        "transferAmount": -392160,
+        "row": {
+          "rowNumber": 17,
+          "switchIdentifiers": "1398 4213 fsp2439",
+          "balance": 5469188,
+          "transferAmount": -392160
+        },
       },
       {
         "participant": {
@@ -155,7 +239,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 4858,
         "balance": 3794243,
-        "transferAmount": -294242
+        "transferAmount": -294242,
+        "row": {
+          "rowNumber": 18,
+          "switchIdentifiers": "1593 4858 fsp1135",
+          "balance": 3794243,
+          "transferAmount": -294242
+        },
       },
       {
         "participant": {
@@ -164,7 +254,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 3640,
         "balance": 1627966,
-        "transferAmount": 367050
+        "transferAmount": 367050,
+        "row": {
+          "rowNumber": 19,
+          "switchIdentifiers": "3311 3640 fsp292",
+          "balance": 1627966,
+          "transferAmount": 367050
+        },
       },
       {
         "participant": {
@@ -173,7 +269,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 889,
         "balance": 3223778,
-        "transferAmount": 360697
+        "transferAmount": 360697,
+        "row": {
+          "rowNumber": 20,
+          "switchIdentifiers": "1708 889 fsp3852",
+          "balance": 3223778,
+          "transferAmount": 360697
+        },
       },
       {
         "participant": {
@@ -182,7 +284,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 683,
         "balance": 1019530,
-        "transferAmount": -37979
+        "transferAmount": -37979,
+        "row": {
+          "rowNumber": 21,
+          "switchIdentifiers": "549 683 fsp2499",
+          "balance": 1019530,
+          "transferAmount": -37979
+        },
       },
       {
         "participant": {
@@ -191,7 +299,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 1989,
         "balance": 2032587,
-        "transferAmount": 109000
+        "transferAmount": 109000,
+        "row": {
+          "rowNumber": 22,
+          "switchIdentifiers": "917 1989 fsp4341",
+          "balance": 2032587,
+          "transferAmount": 109000
+        },
       },
       {
         "participant": {
@@ -200,7 +314,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 1912,
         "balance": 6124080,
-        "transferAmount": -492349
+        "transferAmount": -492349,
+        "row": {
+          "rowNumber": 23,
+          "switchIdentifiers": "976 1912 fsp4095",
+          "balance": 6124080,
+          "transferAmount": -492349
+        },
       },
       {
         "participant": {
@@ -209,16 +329,28 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 1426,
         "balance": 3167831,
-        "transferAmount": 339714
+        "transferAmount": 339714,
+        "row": {
+          "rowNumber": 24,
+          "switchIdentifiers": "2723 1426 fsp2026",
+          "balance": 3167831,
+          "transferAmount": 339714
+        },
       },
       {
         "participant": {
           "id": 3226,
-          "name": "fsp3446",
+          "name": "fsp3446"
         },
         "positionAccountId": 3590,
         "balance": 3375536,
-        "transferAmount": -443104
+        "transferAmount": -443104,
+        "row": {
+          "rowNumber": 25,
+          "switchIdentifiers": "3226 3590 fsp3446",
+          "balance": 3375536,
+          "transferAmount": -443104
+        },
       },
       {
         "participant": {
@@ -227,7 +359,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 1688,
         "balance": 7581499,
-        "transferAmount": 40997
+        "transferAmount": 40997,
+        "row": {
+          "rowNumber": 26,
+          "switchIdentifiers": "4647 1688 fsp3986",
+          "balance": 7581499,
+          "transferAmount": 40997
+        },
       },
       {
         "participant": {
@@ -236,7 +374,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 4812,
         "balance": 723064,
-        "transferAmount": -353399
+        "transferAmount": -353399,
+        "row": {
+          "rowNumber": 27,
+          "switchIdentifiers": "4615 4812 fsp3301",
+          "balance": 723064,
+          "transferAmount": -353399
+        },
       },
       {
         "participant": {
@@ -245,7 +389,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 1713,
         "balance": 387785,
-        "transferAmount": 37653
+        "transferAmount": 37653,
+        "row": {
+          "rowNumber": 28,
+          "switchIdentifiers": "3182 1713 fsp4384",
+          "balance": 387785,
+          "transferAmount": 37653
+        },
       },
       {
         "participant": {
@@ -254,7 +404,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 570,
         "balance": 4002874,
-        "transferAmount": 160961
+        "transferAmount": 160961,
+        "row": {
+          "rowNumber": 29,
+          "switchIdentifiers": "3055 570 fsp3446",
+          "balance": 4002874,
+          "transferAmount": 160961
+        },
       },
       {
         "participant": {
@@ -263,7 +419,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 1482,
         "balance": 1016880,
-        "transferAmount": 292990
+        "transferAmount": 292990,
+        "row": {
+          "rowNumber": 30,
+          "switchIdentifiers": "650 1482 fsp795",
+          "balance": 1016880,
+          "transferAmount": 292990
+        },
       },
       {
         "participant": {
@@ -272,7 +434,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 4417,
         "balance": 4021754,
-        "transferAmount": -432234
+        "transferAmount": -432234,
+        "row": {
+          "rowNumber": 31,
+          "switchIdentifiers": "3486 4417 fsp2553",
+          "balance": 4021754,
+          "transferAmount": -432234
+        },
       },
       {
         "participant": {
@@ -281,7 +449,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 841,
         "balance": 8832735,
-        "transferAmount": 30895
+        "transferAmount": 30895,
+        "row": {
+          "rowNumber": 32,
+          "switchIdentifiers": "36 841 fsp1176",
+          "balance": 8832735,
+          "transferAmount": 30895
+        },
       },
       {
         "participant": {
@@ -290,7 +464,13 @@ test('simple_positive_2.xlsx', async () => {
         },
         "positionAccountId": 2916,
         "balance": 4548331,
-        "transferAmount": 218504
+        "transferAmount": 218504,
+        "row": {
+          "rowNumber": 33,
+          "switchIdentifiers": "1674 2916 fsp1399",
+          "balance": 4548331,
+          "transferAmount": 218504
+        },
       },
     ],
   });
@@ -324,6 +504,7 @@ test('missing_mojaloop_identifier.xlsx', async () => {
   const f = await readFile(path.join(__dirname, '/mock_data/missing_mojaloop_identifier.xlsx'));
   const report = await deserializeReport(f);
   expect(report).toEqual({
+    "settlementId": 13,
     "entries": [
       {
         "balance": 1501000,
@@ -332,33 +513,26 @@ test('missing_mojaloop_identifier.xlsx', async () => {
           "name": "mmdokdollar"
         },
         "positionAccountId": 21,
-        "transferAmount": -1500
+        "row": {
+          "balance": 1501000,
+          "rowNumber": 7,
+          "switchIdentifiers": "11 21 mmdokdollar",
+          "transferAmount": -1500,
+        },
+        "transferAmount": -1500,
       }
     ],
-    "settlementId": 13
   });
-});
-
-test('bad_balance_column_type.xlsx', async () => {
-  expect.assertions(1);
-  const f = await readFile(path.join(__dirname, '/mock_data/bad_balance_column_type.xlsx'));
-  await expect(deserializeReport(f)).rejects.toThrow(/^Unable to extract account balance from C8. Cell data type is not numeric.$/);
 });
 
 test('missing_balance_entry.xlsx', async () => {
   expect.assertions(1);
   const f = await readFile(path.join(__dirname, '/mock_data/missing_balance_entry.xlsx'));
-  await expect(deserializeReport(f)).rejects.toThrow(/^Unable to extract account balance from C9. Cell data type is not numeric.$/);
-});
-
-test('bad_transfer_amount_column_type.xlsx', async () => {
-  expect.assertions(1);
-  const f = await readFile(path.join(__dirname, '/mock_data/bad_transfer_amount_column_type.xlsx'));
-  await expect(deserializeReport(f)).rejects.toThrow(/^Unable to extract transfer amount from D7. Cell data type is not numeric.$/);
+  await expect(deserializeReport(f)).rejects.toThrow(/^Unable to extract account balance from C9. Cell contents: \[\]$/);
 });
 
 test('missing_transfer_amount.xlsx', async () => {
   expect.assertions(1);
   const f = await readFile(path.join(__dirname, '/mock_data/missing_transfer_amount.xlsx'));
-  await expect(deserializeReport(f)).rejects.toThrow(/^Unable to extract transfer amount from D8. Cell data type is not numeric.$/);
+  await expect(deserializeReport(f)).rejects.toThrow(/^Unable to extract transfer amount from D8. Cell contents: \[\]$/);
 });
