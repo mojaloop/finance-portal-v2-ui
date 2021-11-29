307bad1ff242b10e4e834f391f540355ce46ff58
msk- <mattkingston@gmail.com>
2021-11-29 14:02:21 +0000

    Expand/improve lib

--- integration_test/e2e-ui-tests/src/lib/api/index.ts
+++ integration_test/e2e-ui-tests/src/lib/api/index.ts
@@ -1,8 +1,19 @@
 
 import reporting from './reporting';
-import settlement from'./settlement';
+import settlement from './settlement';
+import { protocol } from 'mojaloop-voodoo-client';
+
+export enum ResponseKind {
+  MojaloopError,
+  Okay,
+}
+
+export type MLApiResponse<T> =
+  | { kind: ResponseKind.MojaloopError; body: protocol.ErrorResponse }
+  | { kind: ResponseKind.Okay; body: T };
 
 export default {
   reporting,
   settlement,
+  ResponseKind,
 }
--- integration_test/e2e-ui-tests/src/lib/api/settlement.ts
+++ integration_test/e2e-ui-tests/src/lib/api/settlement.ts
@@ -1,18 +1,78 @@
 
-import got, { OptionsOfBufferResponseBody, CancelableRequest, Response } from 'got';
+import got, { OptionsOfJSONResponseBody } from 'got';
+import { protocol } from 'mojaloop-voodoo-client';
+import { URLSearchParams } from 'url';
 
-// TODO: should accept basePath: URL | string
-export async function getSettlementInitiationReport(basePath: string, settlementId: number): Promise<CancelableRequest<Response<Buffer>>> {
-  const bufferResponseOpts: OptionsOfBufferResponseBody = {
-    responseType: 'buffer',
+export enum ResponseKind {
+  MojaloopError,
+  Okay,
+}
+
+type MLApiResponse<T> =
+  | { kind: ResponseKind.MojaloopError; body: protocol.ErrorResponse }
+  | { kind: ResponseKind.Okay; body: T };
+
+interface SettlementsQueryBase {
+  currency?: protocol.Currency;
+  participantId?: protocol.ParticipantId;
+  settlementWindowId?: protocol.SettlementWindowId;
+  accountId?: protocol.ParticipantCurrencyId;
+  state?: protocol.SettlementState;
+  fromDateTime?: protocol.DateTime;
+  toDateTime?: protocol.DateTime;
+  fromSettlementWindowDateTime?: protocol.DateTime;
+  toSettlementWindowDateTime?: protocol.DateTime;
+}
+interface SettlementsQueryCurrencyRequired extends SettlementsQueryBase { currency: protocol.Currency }
+interface SettlementsQueryParticipantIdRequired extends SettlementsQueryBase { participantId: protocol.ParticipantId }
+interface SettlementsQuerySettlementWindowIdRequired extends SettlementsQueryBase { settlementWindowId: protocol.SettlementWindowId }
+interface SettlementsQueryAccountIdRequired extends SettlementsQueryBase { accountId: protocol.ParticipantCurrencyId }
+interface SettlementsQueryStateRequired extends SettlementsQueryBase { state: protocol.SettlementState }
+interface SettlementsQueryFromDateTimeRequired extends SettlementsQueryBase { fromDateTime: protocol.DateTime }
+interface SettlementsQueryToDateTimeRequired extends SettlementsQueryBase { toDateTime: protocol.DateTime }
+interface SettlementsQueryFromSettlementWindowDateTimeRequired extends SettlementsQueryBase { fromSettlementWindowDateTime: protocol.DateTime }
+interface SettlementsQueryToSettlementWindowDateTimeRequired extends SettlementsQueryBase { toSettlementWindowDateTime: protocol.DateTime }
+
+type GetSettlementsRequest =
+  | SettlementsQueryCurrencyRequired
+  | SettlementsQueryParticipantIdRequired
+  | SettlementsQuerySettlementWindowIdRequired
+  | SettlementsQueryAccountIdRequired
+  | SettlementsQueryStateRequired
+  | SettlementsQueryFromDateTimeRequired
+  | SettlementsQueryToDateTimeRequired
+  | SettlementsQueryFromSettlementWindowDateTimeRequired
+  | SettlementsQueryToSettlementWindowDateTimeRequired;
+
+export async function getSettlements(basePath: string, query: GetSettlementsRequest): Promise<MLApiResponse<protocol.Settlement[]>> {
+  const opts: OptionsOfJSONResponseBody = {
     isStream: false,
     resolveBodyOnly: false,
+    responseType: 'json',
+    throwHttpErrors: false,
+    searchParams: new URLSearchParams(Object.entries(query)),
   };
-  return got.get(`${basePath}/settlementInitiation.xlsx?settlementId=${settlementId}`, {
-    ...bufferResponseOpts,
-  });
+  const result = await got.get<protocol.Settlement[] | protocol.ErrorResponse>(`${basePath}/v2/settlements`, opts);
+  if (result.statusCode >= 200 && result.statusCode < 300) {
+    return {
+      kind: ResponseKind.Okay,
+      body: result.body as protocol.Settlement[],
+    };
+  }
+  // Need to handle badly typed response from central settlement API
+  // See: https://github.com/mojaloop/project/issues/2344
+  if (result.statusCode === 400 && (result.body as protocol.ErrorResponse).errorInformation !== undefined) {
+    return {
+      kind: ResponseKind.Okay,
+      body: [],
+    };
+  }
+  return {
+    kind: ResponseKind.MojaloopError,
+    body: result.body as protocol.ErrorResponse,
+  };
 }
 
 export default {
-  getSettlementInitiationReport,
+  getSettlements,
 }
